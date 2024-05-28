82730886464b7245ac035e25f1f518b749039484
DENIZSEK <161316112+DENIZSEKER99@users.noreply.github.com>
2024-05-28 03:30:45 +0300

    music and score added

--- scenes/Puzzle_Area.gd
+++ scenes/Puzzle_Area.gd
@@ -4,16 +4,23 @@ class_name puzzle_area
 var tiles = []
 var solved = []
 var mouse = false
-
 var hardness = 50
-
 var sound_is_on = true
+var music_is_on =true
+var moves = 0
+static var min_moves
+var min_moves_is_null = true
+
 
 func _ready():
+	if music_is_on:
+		$MusicStreamPlayer.play()
 	start_game()
 
 func start_game():
-	tiles = [$Tile1, $Tile2, $Tile3, $Tile4, $Tile5, $Tile6, $Tile7, $Tile8, $Tile9, $Tile10, $Tile11, $Tile12, $Tile13, $Tile14, $Tile15, $Tile16 ]
+	tiles = [$Tile1, $Tile2,
+	$Tile3, $Tile4, $Tile5, $Tile6,$Tile7, $Tile8, $Tile9,
+	$Tile10, $Tile11, $Tile12, $Tile13, $Tile14, $Tile15, $Tile16 ]
 	solved = tiles.duplicate()
 	shuffle_tiles()
 
@@ -28,6 +35,7 @@ func shuffle_tiles():
 			check_neighbours(rows,cols)
 			previous_1 = previous
 			previous = tile
+			moves=0
 
 func hard_shuffle_tiles():
 	var previous = 99
@@ -40,6 +48,7 @@ func hard_shuffle_tiles():
 			check_neighbours(rows,cols)
 			previous_1 = previous
 			previous = tile
+			moves=0
 
 func _process(delta):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and mouse:
@@ -50,14 +59,22 @@ func _process(delta):
 		var cols = int(mouse_copy.position.x / 250)
 		check_neighbours(rows,cols)
 		if tiles == solved:
+			if min_moves_is_null:
+				min_moves = moves
+				$Highest_Score.text = str(min_moves)
+				min_moves_is_null = false
+			elif min_moves > moves:
+				min_moves = moves
+				$Highest_Score.text = str(min_moves)
+				min_moves_is_null = false
 			if sound_is_on:
 				$AudioStreamPlayer.stream=preload("res://art/audio/Small Win.mp3")
 				$AudioStreamPlayer.play()
 				pass
-			print("You win!")
 			var you_win = preload("res://scenes/You win.tscn")
 			var win_instance = you_win.instantiate()
 			add_child(win_instance)
+			$Timer.start()
 
 func check_neighbours(rows, cols):
 	var empty = false
@@ -100,6 +117,8 @@ func swap_tiles(tile_src, tile_dst):
 	var temp_tile = tiles[tile_src]
 	tiles[tile_src] = tiles[tile_dst]
 	tiles[tile_dst] = temp_tile
+	moves = moves+1
+	$Current_Score.text=str(moves)
 	if sound_is_on:
 		$AudioStreamPlayer.stream = preload("res://art/audio/Whip Woosh.mp3")
 		$AudioStreamPlayer.play()
@@ -124,3 +143,27 @@ func _on_sound_button_button_down():
 		$Sound_Button.icon = preload("res://art/Icons/Sound_on.png")
 		sound_is_on = true
 	pass
+
+func _on_timer_timeout():
+	remove_child($Win_Screen)
+	remove_child($Win_Screen1)
+	remove_child($Win_Screen2)
+	remove_child($Win_Screen3)
+	pass
+
+func _on_music_button_button_down():
+	if music_is_on:
+		$Music_Button.icon=preload("res://art/Icons/music_off.png")
+		music_is_on=false
+		$MusicStreamPlayer.stop()
+	else:
+		$Music_Button.icon =preload("res://art/Icons/music_on.png")
+		music_is_on = true
+		$MusicStreamPlayer.play()
+	pass
+
+
+func _on_restart_button_down():
+	
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	pass # Replace with function body.
